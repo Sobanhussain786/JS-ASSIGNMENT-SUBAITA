11f2eaf84df8a22c42ddbdd00201bac46954863d
Sobanhussain786 <hussainsubhan876@gmail.com>
2025-09-19 15:45:32 +0530

    JS-ASSIGNMENT-2

---
+++ js subaita2/app.js
@@ -0,0 +1,448 @@
+ function greet() {
+    console.log("Hello from LOCAL code!");
+    console.log("Hello from REMOTE code!");
+}
+
+               // CHAPTER 1   
+   
+//   Q2
+//  alert("Error!Please enter a valid password");
+
+// Q3
+//   alert("Welcome to JS Land \n Happy Coding!");
+
+// Q4
+// alert("Welcome to JS Land");
+// alert("Happy Coding");
+
+// Q5
+//  var names="Hello I can run JS through my web browser console";
+// console.log(names);
+
+// Q6
+// alert("Welcome to the My Website");
+
+            //   CHAPTER 2
+
+// Q1    
+// var user="username";
+// console.log(user);
+
+// Q2
+//  var fullName="Soban ";
+//  var lastName="M.Hussain"
+//  console.log(fullName,lastName);
+
+// Q3 (a)
+// var title="message";
+// console.log(title);
+
+// (b) 
+// var message="Hello World";
+// console.log(message);
+
+// (c)
+// var message="Hello World";
+// alert(message);
+
+// Q.4
+//  var names="Bilal Hussain";
+//  alert(names);
+//  var age="15 years old";
+//  alert(age);
+//  var qualification="Certified Mobile Application Development";
+//  alert(qualification);
+
+// Q.5
+// var names="PIZZA \n PIZZ \n PIZ \n PI \n P";
+// alert(names);
+
+// Q.6
+// var names="My email address is ";
+// var email="example@example.com";
+// alert(names+""+email);
+
+// Q.7
+// var book="A smarter way to learn javascrpit";
+// alert(book);
+
+// Q.8
+// document.write("Yah! I can write HTML content through JAVASCRIPT");
+
+// Q9
+// var names="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(names);
+
+              //    CHAPTER 3
+
+// Q1
+// var age="I AM 15 Year Old";
+// alert(age);
+
+// Q2
+// var visit="You have visited this site 15 times";
+// alert (visit);
+
+// Q3
+// document.write("My Birth Year is 2007");
+
+// Q4
+// document.write("John Doe Ordered 5T-shirt(s) on XYZ clothing store");
+
+                 // CHAPTER 4
+
+// Q1
+// var names,age,email;
+
+// Q2
+// var username,user123,age,$price,_city;
+// var 123username,@email,124,user name,user-name;
+
+// Q3
+// document.write("Variable names can only contain,numbers,$ and_For example:e $my_1stVariable"); 
+
+// document.write("Variable names can only contain,numbers,$ and_For example:e $my_1stVariable ");
+
+
+                 // // CHAPTER 5
+
+// Q1
+// var num1=6;
+// var num2=8;
+// var sum=num1+num2
+// document.write(sum);
+
+// Q2  (FOR SUBTRACTION)
+// var num1=12;
+//  var num2=8;
+//  var sum=num1-num2;
+//  document.write(sum)
+
+// (FOR MULTIPLICATION)
+//  var num1=12;
+//  var num2=8;
+//  var sum=num1*num2;
+//  document.write(sum);
+
+//  (FOR DIVISION)
+//  var num1=12;
+//  var num2=4;
+//  var sum=num1/num2;
+//  document.write(sum);
+
+// (FOR MODULUS)
+//  var num1=12;
+//  var num2=8;
+//  var sum=num1%num2;
+//  document.write(sum);
+
+// Q3 (a)
+// var sum;
+// document.write(sum + "<br>");
+// // (b)
+// var num = 8;
+// document.write(num + "<br>");
+// // (c)
+// var num4=5;
+// document.write("initial value is "+ num4 +"<br>" );
+// // (d)
+// var g=5 + ++x;
+// document.write("increment value is "+ g );
+
+// Q5
+// var userInput = Number(prompt("Enter a number"));
+
+// document.write(userInput, " x 1 = ", userInput * 1, "<br>",
+//     userInput, " x 2 = ", userInput * 2, "<br>",
+//     userInput, " x 3 = ", userInput * 3, "<br>",
+//     userInput, " x 4 = ", userInput * 4, "<br>",
+//     userInput, " x 5 = ", userInput * 5, "<br>",
+//     userInput, " x 6 = ", userInput * 6, "<br>",
+//     userInput, " x 7 = ", userInput * 7, "<br>",
+//     userInput, " x 8 = ", userInput * 8, "<br>",
+//     userInput, " x 9 = ", userInput * 9, "<br>",
+//     userInput, " x 10 = ", userInput * 10, "<br>"
+// );
+
+// Q6
+// var celsiusTemp = prompt("Enter celsius temperature");
+
+// var FahrenheitTemp = prompt("Enter Fahrenheit temperature");
+
+// var convertF = (celsiusTemp * 9) / 5 + 32;
+
+// var convertC = ((FahrenheitTemp - 32) * 5) / 9;
+
+// console.log("Fahrenheit ", convertF);
+
+// console.log("Celsius ", convertC);
+
+
+            // CHAPTER.06-09
+
+// Q#2 
+//  var a =2,b =1; 
+//  var c =--a;
+//  console.log(c);
+
+//  var d = --a - --b;
+//  console.log(d);
+
+//  var e=--a - --b + ++b;
+//  console.log(e);
+
+//  var f=--a - --b + ++b + b--;
+//  console.log(f);
+
+// Q#3
+//  var hello="Hello User";
+//  alert(hello);
+//  var username =prompt("Enter User Name");
+
+// Q#5
+//  var userInput = Number(prompt("Enter a number") || 5);
+
+//  document.write(userInput, " x 1 = ", userInput * 1, "<br>",
+//      userInput, " x 2 = ", userInput * 2, "<br>",
+//     userInput, " x 3 = ", userInput * 3, "<br>",
+//     userInput, " x 4 = ", userInput * 4, "<br>",
+//     userInput, " x 5 = ", userInput * 5, "<br>",
+//     userInput, " x 6 = ", userInput * 6, "<br>",
+//     userInput, " x 7 = ", userInput * 7, "<br>",
+//      userInput, " x 8 = ", userInput * 8, "<br>",
+//      userInput, " x 9 = ", userInput * 9, "<br>",
+//      userInput, " x 10 = ", userInput * 10, "<br>");
+
+
+
+
+
+        //    CHAPTER 9-11
+
+// Q#1
+// var city= prompt("Enter your city name");
+// if(city= "karachi"){
+//     alert("Welcome to city of lights")
+// };
+
+// Q#2
+// var gender= prompt("Enter your gender");
+// if(gender == "male"){
+//     document.write("Good Morning Sir")
+// }
+// else if(gender == "female")
+//    {document.write("Good Morning Madam")
+// };
+
+// Q#3
+// var color= prompt("Enter road traffic signal color");
+// if( color == "red"){
+//     document.write("Must Stop")
+// }
+// else if(color == "yellow"){
+//     document.write("Ready To Go")
+// }
+// else if(color == "green"){
+//     document.write("Move Now")
+// }
+
+// Q#4
+// var fuel=prompt("Enter remaining fuel in car")
+// if(fuel < "0.25litres"){
+//     document.write("Please refill the fuel in your car")
+// }
+
+//  Q#6
+//  var mark1= +prompt("Marks Obtained in first sub");
+//  var mark2= +prompt("Marks Obtained in second sub");
+//  var mark3= +prompt("Marks Obtained in third sub");
+//  var totalmarks=400
+//  var obtainedmarks= mark1 + mark2 + mark3;
+//  var Percent="74"+"%";
+//  var grade= "B";
+//  var remarks= "Good";
+
+// document.write("<h1>Marks sheet</h1><br>","Total Marks:" +totalmarks + "<br>","Marks Obtained:"+obtainedmarks + "<br>"+"Percentage:"+Percent + "<br>"+"Grade:"+grade + "<br>" +"Remarks:"+remarks
+// )
+
+// Q#7
+//  var num=4;
+//  var guess= +prompt("Gues the secret number");
+//  if(guess==4){
+//      document.write("Bingo Correct Answer")
+//  }
+//  else if(guess +"4"){
+//      document.write("Close Enough To The Correct Answer")
+//  }
+
+// Q#8
+// var num= +prompt("Give Number");
+// if(num / "3"){
+//     document.write("Divisible by 3")
+// }
+// else if(num == "7"){
+//     document.write("Not divisible by 3")
+// }
+
+// Q9
+//  var number=+prompt("Enter a number");
+//  if(number % 2 ===0){
+//     document.write("This is a Even number");
+//  } else{
+//     document.write("This is a Odd number")
+//  }
+
+// }
+// Q10
+// var temperature=+prompt("Write a temperature");
+// if (temperature >40){
+//     document.write("It is too hot outside.");
+// } else if (temperature >30){
+//     document.write("The Weather today is Normal");
+// } else if (temperature >20){
+//     document.write("“Today’s Weather is cool");
+// }    else if (temperature >10){
+//     document.write("OMG! Today’s weather is so Cool");
+// }    
+
+// Q11
+
+// var num1 = +prompt("Enter first number:");
+// var num2 = +prompt("Enter second number:");
+// var operation = prompt("Enter operation (+, -, *, /, %):");
+
+// var result;
+
+
+// if (operation === "+") {
+//     result = num1 + num2;
+// }
+// if (operation === "-") {
+//     result = num1 - num2;
+// }
+// if (operation === "*") {
+//     result = num1 * num2;
+// }
+// if (operation === "/") {
+//     result = num1 / num2;
+// }
+// if (operation === "%") {
+//     result = num1 % num2;
+// }
+
+
+// document.write("First Number: " + num1 + "<br>");
+// document.write("Second Number: " + num2 + "<br>");
+// document.write("Operation: " + operation + "<br>");
+// document.write("Result: " + result);
+
+// Chap 12-13
+// Q1
+// var char = prompt("Enter a character:");
+// var ascii = char.charCodeAt(0);
+
+// if (ascii >= 48 && ascii <= 57) {
+//     document.write("You entered a Number");
+// }
+// else if (ascii >= 65 && ascii <= 90) {
+//     document.write("You entered an Uppercase Letter");
+// }
+// else if (ascii >= 97 && ascii <= 122) {
+//     document.write("You entered a Lowercase Letter");
+// }
+// else {
+//     document.write("You entered a Special Character");
+// }
+
+// Q2
+// var num1= + prompt("Enter  first Number");
+// var num2= + prompt("Enter second Number");
+
+// if(num1 > num2 ){
+//     document.write(num1 + " larger then " + num2);
+// } else if(num2 > num1){
+//     document.write(num2 + " Larger then" + num1);
+// } else{
+//     document.write("Both numbers are equal");
+// }
+// Q3
+// var userNumber= +prompt("Enter Number");
+
+// if(userNumber > 0){
+//     document.write(userNumber + "  is a positive number");
+// } else if (userNumber < 0){
+//     document.write(userNumber + "  is a negative number" );
+// } else {
+//     document.write("This number is zero");
+// }
+
+// Q4
+// var userVowel=prompt("Enter a  Alphabet");
+
+// if(userVowel === "a"){
+//     console.log("This alphabet is a vowel");
+// } else if (userVowel === "e"){
+//     console.log("This alphabet is  a vowel");
+// } else if (userVowel === "i"){
+//      console.log("This alphabet is  a vowel");
+// }  else if (userVowel === "o"){
+//     console.log("This alphabet is  a vowel");
+// }   else if (userVowel === "u"){
+//     console.log("This alphabet is  a vowel");
+// } else {
+//      console.log("This alphabet is  not a vowel");
+// } 
+
+// Q5
+// var userPassword="sob@n786";
+// var password=prompt("Enter your password");
+
+// if (password === "" || password === null){
+//     console.log("Please Enter Your Password");
+// }  else if (password == userPassword){
+//     console.log("Correct");
+// } else {
+//     console.log("Incorrect Password");
+// }
+
+// Q6
+// var hour= +prompt("Enter Hour");
+
+// if(hour <18){
+//     console.log("Good Day");
+// } else {
+//     console.log("Goof Evening");
+// }
+
+// Q7
+// var time= +prompt("Enter Time");
+
+// if(time ==1300){
+//     console.log(" 1PM ");
+// } else if (time ==1400){
+//     console.log(" 2PM ");
+// }  else if (time ==1500){
+//     console.log("3PM");
+// } else if (time ==1600){
+//     console.log("4PM");
+// } else if (time ==1700){
+//     console.log("5PM"); 
+// } else if (time ==1800){
+//     console.log("6PM");     
+// } else if (time ==1900){
+//     console.log("7PM");
+// } else if (time ==2000){
+//     console.log("8PM");
+// } else if (time ==2100){
+//     console.log("9PM");
+// } else if (time ==2200){
+//     console.log("10PM");
+// } else if (time ==2300){
+//     console.log("11PM");  
+// } else if (time ==2400){
+//     console.log("12PM"); 
+// }                 
+
+
+
+
